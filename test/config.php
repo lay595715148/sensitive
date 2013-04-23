76a27ac18d19780a16b2a3f3f6936df1eb478d5f
wminjay <lay595715148@126.com>
2013-04-23 14:11:22 +0800

    change test/config.php

--- test/config.php
+++ test/config.php
@@ -1,22 +1,22 @@
 <?php
 global $_CFG;
 
-$_CFG['actions']['index']['classname'] = 'DefaultAction';
-$_CFG['actions']['index']['services'] = array('default');
-$_CFG['actions']['index']['beans'] = array('default');
+$_CFG['actions']['t']['classname'] = 'DefaultAction';
+$_CFG['actions']['t']['services'] = array('default');
+$_CFG['actions']['t']['beans'] = array('default');
 
 $_CFG['beans']['default']['classname'] = 'DefaultBean';
 
 $_CFG['services']['default']['classname'] = 'DefaultService';
-$_CFG['services']['default']['store'] = 'default';
+$_CFG['services']['default']['store'] = 'mysql';
 
-$_CFG['stores']['default']['classname'] = 'Mysql';
-$_CFG['stores']['default']['host'] = '127.0.0.1';
-$_CFG['stores']['default']['username'] = 'root';
-$_CFG['stores']['default']['password'] = 'dcuxpasswd';
-$_CFG['stores']['default']['database'] = 'sso';
-$_CFG['stores']['default']['encoding'] = 'UTF8';
-$_CFG['stores']['default']['showsql'] = true;
+$_CFG['stores']['mysql']['classname'] = 'Mysql';
+$_CFG['stores']['mysql']['host'] = '127.0.0.1';
+$_CFG['stores']['mysql']['username'] = 'root';
+$_CFG['stores']['mysql']['password'] = 'dcuxpasswd';
+$_CFG['stores']['mysql']['database'] = 'sso';
+$_CFG['stores']['mysql']['encoding'] = 'UTF8';
+$_CFG['stores']['mysql']['showsql'] = true;
 
 $_CFG['mapping']['tables']['DefaultTableBean'] = 'default';
 
