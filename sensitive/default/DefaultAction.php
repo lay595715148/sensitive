11feff71a47f84f2e8bfe07b02e4b4bb5a50c0f8
wminjay <lay595715148@126.com>
2013-05-09 14:37:03 +0800

    liaiyong : modify DefaultAction

--- sensitive/default/DefaultAction.php
+++ sensitive/default/DefaultAction.php
@@ -6,7 +6,7 @@ class DefaultAction extends AbstractAction {
         global $_SRCPath;
         $this->template->css('404.css?_r='.rand());
         $this->template->push('title','Page not found · Sensitive');
-        $this->template->file($_SRCPath.'/test/templates/404.php');
+        $this->template->template('/404.php');
     }
     public function tail() {//must return $this
         $this->template->out();
