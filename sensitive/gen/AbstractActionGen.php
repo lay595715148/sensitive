4f4401260429f5c051b01f60565d4afcd6a18c82
lay595715148 <lay595715148@126.com>
2013-07-24 13:45:29 +0800

    Update AbstractActionGen.php

--- sensitive/gen/AbstractActionGen.php
+++ sensitive/gen/AbstractActionGen.php
@@ -1,7 +1,23 @@
 <?php
+/**
+ * 控制器对象生成器
+ * @see https://github.com/lay595715148/sensitive
+ * 
+ * @author liaiyong<595715148@qq.com>
+ * @Version: 0.1.48 (build 130723)
+ */
 if(!defined('INIT_SENSITIVE')) { exit; }
 
+/**
+ * 控制器对象生成器
+ * @abstract
+ */
 abstract class AbstractActionGen extends AbstractGen {
+    /**
+	 * 生成控制器对象
+	 * @abstract
+	 * @param string $keyword 关键字
+	 */
     public abstract function genAction($keyword = '');
 }
 ?>
